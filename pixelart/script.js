69fe9b2ec6d78356a86b120625259528ec48eeb7
Quelvinmp <quelvinmp@gmail.com>
2022-12-22 16:31:37 -0300

    matriz tamanho estático e pixels relativos

--- pixelart/script.js
+++ pixelart/script.js
@@ -8,7 +8,6 @@ const savedPixels = [];
 
 const pixelBoard = document.getElementById('pixel-board');
 const pixel = document.getElementsByClassName('pixel');
-const pixelFather = document.getElementById('pixel-father');
 
 const initialColor = document.querySelector('.color');
 const initialSelectedColor = document.getElementsByClassName('selected');
@@ -83,8 +82,8 @@ const createPixelsInBoard = (size) => {
       const divColumn = document.createElement('div');
       divColumn.classList.add('pixel');
       divColumn.classList.add('pixelChild');
-      // divColumn.style.width = `${(500 / size)}px`;
-      // divColumn.style.height = `${(500 / size)}px`;
+      divColumn.style.width = `${(292 / size)}px`;
+      divColumn.style.height = `${(292 / size)}px`;
       // divColumn.style.width = '40px';
       // divColumn.style.height = '40px';
       divColumn.style.backgroundColor = 'white';
